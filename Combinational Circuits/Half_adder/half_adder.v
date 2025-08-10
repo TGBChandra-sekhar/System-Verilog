39fbe94909567f36208f6c4759e07a9755c05955
Tanuku Gnanendra Bala Chandra Sekhar <23a95a0426@aec.edu.in>
2025-08-10 19:28:41 +0530

    Update half_adder.v

--- Combinational Circuits/Half_adder/half_adder.v
+++ Combinational Circuits/Half_adder/half_adder.v
@@ -1,1 +1,17 @@
+//DUT
+module half_adder(
+  input a,b,
+  output sum,cout
+);
+  
+  assign sum = a^b;
+  assign cout = a&b;
+endmodule
+
+//Interface
+interface inter;
+  logic a,b;
+  logic sum,cout;
+endinterface
+
 
